f5f7b4c6a46041dd3c120d152f1fe4eca01c2cdf
Geosalud <63572420+Geosalud@users.noreply.github.com>
2020-04-18 15:46:45 -0500

    Add files via upload

--- mapa/layers/layers.js
+++ mapa/layers/layers.js
@@ -12,24 +12,24 @@ new ol.layer.Tile({
 })
 ]
 });
-var format_PercepcindelapoblacinantelapandemiaCovid19_0 = new ol.format.GeoJSON();
-var features_PercepcindelapoblacinantelapandemiaCovid19_0 = format_PercepcindelapoblacinantelapandemiaCovid19_0.readFeatures(json_PercepcindelapoblacinantelapandemiaCovid19_0, 
+var format_Percepcin_0 = new ol.format.GeoJSON();
+var features_Percepcin_0 = format_Percepcin_0.readFeatures(json_Percepcin_0, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_PercepcindelapoblacinantelapandemiaCovid19_0 = new ol.source.Vector({
+var jsonSource_Percepcin_0 = new ol.source.Vector({
     attributions: [new ol.Attribution({html: '<a href=""></a>'})],
 });
-jsonSource_PercepcindelapoblacinantelapandemiaCovid19_0.addFeatures(features_PercepcindelapoblacinantelapandemiaCovid19_0);var lyr_PercepcindelapoblacinantelapandemiaCovid19_0 = new ol.layer.Vector({
+jsonSource_Percepcin_0.addFeatures(features_Percepcin_0);var lyr_Percepcin_0 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_PercepcindelapoblacinantelapandemiaCovid19_0, 
-                style: style_PercepcindelapoblacinantelapandemiaCovid19_0,
-                title: '<img src="styles/legend/PercepcindelapoblacinantelapandemiaCovid19_0.png" /> Percepción de la población ante la pandemia Covid-19'
+                source:jsonSource_Percepcin_0, 
+                style: style_Percepcin_0,
+                title: '<img src="styles/legend/Percepcin_0.png" /> Percepción'
             });
 
-lyr_PercepcindelapoblacinantelapandemiaCovid19_0.setVisible(true);
-var layersList = [baseLayer,lyr_PercepcindelapoblacinantelapandemiaCovid19_0];
-lyr_PercepcindelapoblacinantelapandemiaCovid19_0.set('fieldAliases', {'ObjectID': 'ObjectID', 'Fecha': 'Fecha', 'Percepcion': 'Percepcion', 'Otro': 'Otro', 'Ocupacion': 'Ocupacion', 'Opinion': 'Opinion', 'Trabajo': 'Trabajo', 'x': 'x', 'y': 'y', });
-lyr_PercepcindelapoblacinantelapandemiaCovid19_0.set('fieldImages', {'ObjectID': 'TextEdit', 'Fecha': 'TextEdit', 'Percepcion': 'TextEdit', 'Otro': 'TextEdit', 'Ocupacion': 'TextEdit', 'Opinion': 'TextEdit', 'Trabajo': 'TextEdit', 'x': 'TextEdit', 'y': 'TextEdit', });
-lyr_PercepcindelapoblacinantelapandemiaCovid19_0.set('fieldLabels', {'ObjectID': 'no label', 'Fecha': 'no label', 'Percepcion': 'no label', 'Otro': 'no label', 'Ocupacion': 'no label', 'Opinion': 'no label', 'Trabajo': 'no label', 'x': 'no label', 'y': 'no label', });
-lyr_PercepcindelapoblacinantelapandemiaCovid19_0.on('precompose', function(evt) {
+lyr_Percepcin_0.setVisible(true);
+var layersList = [baseLayer,lyr_Percepcin_0];
+lyr_Percepcin_0.set('fieldAliases', {'ObjectID': 'ObjectID', 'Fecha': 'Fecha', 'Percepcion': 'Percepcion', 'Otro': 'Otro', 'Ocupacion': 'Ocupacion', 'Opinion': 'Opinion', 'Trabajo': 'Trabajo', 'x': 'x', 'y': 'y', });
+lyr_Percepcin_0.set('fieldImages', {'ObjectID': 'TextEdit', 'Fecha': 'TextEdit', 'Percepcion': 'TextEdit', 'Otro': 'TextEdit', 'Ocupacion': 'TextEdit', 'Opinion': 'TextEdit', 'Trabajo': 'TextEdit', 'x': 'TextEdit', 'y': 'TextEdit', });
+lyr_Percepcin_0.set('fieldLabels', {'ObjectID': 'no label', 'Fecha': 'no label', 'Percepcion': 'no label', 'Otro': 'no label', 'Ocupacion': 'no label', 'Opinion': 'no label', 'Trabajo': 'no label', 'x': 'no label', 'y': 'no label', });
+lyr_Percepcin_0.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
