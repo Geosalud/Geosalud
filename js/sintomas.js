4aec0f3e4b6093d440e4c5568faf3adaf8568e26
Geosalud <63572420+Geosalud@users.noreply.github.com>
2020-04-30 21:05:59 -0500

    Add files via upload

--- js/sintomas.js
+++ js/sintomas.js
@@ -17,7 +17,7 @@ Highcharts.chart('sintomas', {
     xAxis: {
         categories: [
 
-'Tijuana','Iztapalapa','Gustavo A. Madero','Centro','Culiacan','Benito Juarez','Mexicali','Tlalpan','Nezahualcoyotl','Puebla','Ecatepec de Morelos','Alvaro Obregon','Coyoacan','Cuauhtemoc','Merida','Miguel Hidalgo','Iztacalco','Venustiano Carranza','Xochimilco','Benito Juarez','Naucalpan de Juarez','Azcapotzalco','Juarez','Tlalnepantla de Baz','Veracruz','Los Cabos','Monclova','Tlahuac','Chimalhuacan','Aguascalientes','Cuajimalpa de Morelos','Acapulco de Juarez','Milpa Alta','Solidaridad','Chalco','La Magdalena Contreras','Atizapan de Zaragoza','Cuernavaca','Ahome','La Paz','Victoria','Cuautitlan Izcalli'
+'Iztapalapa','Tijuana','Gustavo A. Madero','Benito Juarez','Centro','Culiacan','Mexicali','Tlalpan','Nezahualcoyotl','Ecatepec de Morelos','Puebla','Alvaro Obregon','Coyoacan','Cuauhtemoc','Merida','Iztacalco','Miguel Hidalgo','Xochimilco','Venustiano Carranza','Benito Juarez','Azcapotzalco','Naucalpan de Juarez','Juarez','Tlalnepantla de Baz','Veracruz','Monclova','Los Cabos','Chimalhuacan','Aguascalientes','Tlahuac','La Magdalena Contreras','Acapulco de Juarez','Cuajimalpa de Morelos','Cuernavaca','Chalco','Milpa Alta','Solidaridad','Atizapan de Zaragoza','Ahome','Lazaro Cardenas','Ixtapaluca','La Paz','Cuautitlan Izcalli','Tecamac','Queretaro','Guadalajara','Victoria'
 
 
         ]
@@ -39,7 +39,7 @@ Highcharts.chart('sintomas', {
      
         data: [
 
-856,850,603,575,558,531,530,394,388,348,337,331,313,298,295,276,262,245,237,228,218,207,206,199,186,185,180,162,162,155,136,131,123,121,118,117,114,112,107,104,102,100
+911,883,660,597,597,586,557,454,427,374,355,348,328,328,317,294,286,272,267,244,231,231,217,208,208,196,189,175,167,167,153,142,139,134,130,127,127,119,116,110,107,106,105,104,103,102,102
 
 
 
