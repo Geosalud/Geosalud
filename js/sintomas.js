c154fec20c4f7fad4f8aa2b9c1d3f94493a90171
Geosalud <63572420+Geosalud@users.noreply.github.com>
2020-05-15 21:28:22 -0500

    Add files via upload

--- js/sintomas.js
+++ js/sintomas.js
@@ -15,27 +15,28 @@ Highcharts.chart('sintomas', {
         data: [
 
 
-['CDMX', 'Iztapalapa', 2319],
-['CDMX', 'Gustavo A. Madero', 1529],
-['Baja California', 'Tijuana', 1422],
-['Tabasco', 'Centro', 1241],
-['Baja California', 'Mexicali', 1126],
-['Sinaloa', 'Culiacán', 1024],
-['Edo. Mex', 'Nezahualcoyotl', 968],
-['Edo. Mex', 'Ecatepec', 943],
-['CDMX', 'Tlalpan', 933],
-['Quintana Roo', 'Benito Juárez', 909],
-['CDMX', 'Álvaro Obregón', 758],
-['CDMX', 'Coyoacán', 715],
-['CDMX', 'Cuauhtemoc', 694],
-['CDMX', 'Iztacalco', 681],
-['Puebla.', 'Puebla', 676],
-['Veracruz.', 'Veracruz', 615],
-['CDMX', 'Xochimilco', 614],
-['CDMX', 'Venustiano Carranza', 576],
-['Yucatán', 'Mérida', 570],
-['Edo. Mex', 'Naucalpan', 567],
-['CDMX', 'Miguel Hidalgo', 552]
+['CDMX', 'Iztapalapa', 2479],
+['CDMX', 'Gustavo A. Madero', 1611],
+['Baja California', 'Tijuana', 1462],
+['Tabasco', 'Centro', 1288],
+['Baja California', 'Mexicali', 1254],
+['Sinaloa', 'Culiacán', 1065],
+['CDMX', 'Tlalpan', 1014],
+['Edo. Mex', 'Nezahualcoyotl', 1003],
+['Edo. Mex', 'Ecatepec', 983],
+['Quintana Roo', 'Benito Juárez', 929],
+['CDMX', 'Álvaro Obregón', 800],
+['CDMX', 'Coyoacán', 759],
+['CDMX', 'Cuauhtemoc', 738],
+['CDMX', 'Iztacalco', 730],
+['Puebla', 'Puebla', 713],
+['CDMX', 'Xochimilco', 660],
+['Veracruz', 'Veracruz', 627],
+['CDMX', 'Venustiano Carranza', 626],
+['Edo. Mex', 'Naucalpan', 612],
+['Yucatán', 'Mérida', 604],
+['CDMX', 'Miguel Hidalgo', 584]
+
 
 
 
