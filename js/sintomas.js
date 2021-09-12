6ce2bba879f260bef0ee0d243f9fa34584cea882
Geosalud <63572420+Geosalud@users.noreply.github.com>
2021-09-12 16:45:30 -0500

    Add files via upload

--- js/sintomas.js
+++ js/sintomas.js
@@ -15,29 +15,29 @@ Highcharts.chart('sintomas', {
         data: [
 
 
-
 ['CDMX', 'Iztapalapa', 131417],
-['CDMX', 'Álvaro Obregón', 119333],
+['CDMX', 'Álvaro Obregòn', 119333],
 ['CDMX', 'Gustavo A. Madero', 95019],
 ['CDMX', 'Tlalpan', 80672],
 ['Puebla.', 'Puebla', 60968],
 ['Querétaro.', 'Querétaro', 54291],
-['Guanajuato', 'León', 48081],
+['Tabasco', 'Centro', 48081],
 ['Nuevo León', 'Monterrey', 46732],
-['CDMX', 'Coyoacán', 46367],
-['Tabasco', 'Centro', 44665],
+['Guanajuato', 'León', 46367],
+['CDMX', 'Coyoacán', 44665],
 ['CDMX', 'Xochimilco', 44528],
 ['San Luis Potosí.', 'San Luis Potosí', 44272],
 ['CDMX', 'Tlahuac', 43304],
 ['CDMX', 'Cuauhtemoc', 42548],
-['Edo. Mex', 'Ecatepec', 42043],
-['CDMX', 'Azcapotzalco', 41984],
+['CDMX', 'Azcapotzalco', 42043],
+['Edo. Mex', 'Ecatepec', 41984],
 ['Sonora', 'Hermosillo', 40951],
-['CDMX', 'Venustiano Carranza', 39191],
-['Jalisco', 'Guadalajara', 37526],
+['Jalisco', 'Guadalajara', 39191],
+['CDMX', 'Venustiano Carranza', 37526],
 ['Yucatán', 'Mérida', 35784],
 ['Edo. Mex', 'Nezahualcoyotl', 34770],
 ['CDMX', 'Iztacalco', 33791]
+
 
 
 
